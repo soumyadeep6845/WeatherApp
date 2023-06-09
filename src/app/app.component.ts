9b4f4342213e2e002f5caddfc33c84f27f4a00ae
soumyadeep6845 <soumyadeep.dbsslg@gmail.com>
2023-06-09 12:38:52 +0530

    Fetching real-time data from weather API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService){
+    
+  }
+
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Phoenix').subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      }
+    });
+  }
 }
